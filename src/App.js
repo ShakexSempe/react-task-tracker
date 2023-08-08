e9e1aff1b5ffb0ebd77943f59706c24e3230e78f
Ray-Shakex <bshakex@gmail.com>
2023-08-08 10:56:29 +0200

    react app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = "Shakex Sempe";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>React Task Tracker <br /><span>{name}</span></h1>
+
     </div>
   );
 }
