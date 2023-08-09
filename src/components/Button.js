dc43862bc95f5e6c6b06b6652083019d9c71864d
Ray-Shakex <bshakex@gmail.com>
2023-08-09 12:18:48 +0200

    Button proptypes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,27 @@
-const Button = ({bg, color, text}) => {
-    return <button style={{backgroundColor:bg , color:color, fontWeight:'400'}} className='btn'>{text}</button>
+import PropTypes from 'prop-types'
+
+const Button = ({ bg, color, text, onClick }) => {
+
+    return (
+    <button onClick={onClick}
+            style={{backgroundColor:bg , color:color, fontWeight:'400'}} 
+            className='btn'>{text}
+    </button>
+    )
 }
+
+// BUTTON DEFAULT PROPS
+Button.defaultProps = {
+    color: 'yellow'
+}
+
+// BUTTON PROP-TYPES
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,23 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
+    const onClick = () => {
+        alert("Props btn clicked")
+    }
+
     return ( 
         <header className='header'>
             <h1>{props.title}</h1>
-            {/* <Button bg="#0000ff" color='#fff' text="Add Prop" /> */}
-            <Button bg={gradient.backgroundColor} color={gradient.color} text={gradient.text} />
+            <Button bg={gradient.backgroundColor} color={gradient.color} 
+                    text={gradient.text} 
+                    onClick={onClick} />
         </header>
     )
 }
 
+
+
+// BUTTON CSS STYLE OBJECT
 const gradient = {
     backgroundColor: "#0000ff",
     color: '#ffffff',
