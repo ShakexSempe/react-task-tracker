68d562dbfbb97af13906e6e6a2ba83dc87280c1a
Ray-Shakex <bshakex@gmail.com>
2023-08-11 11:15:34 +0200

    Tasks.js

---
+++ src/components/Tasks.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+const Tasks = () => {
+    const [tasks,setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: 'Write Markup',
+                day: 'Monday',
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: 'Stylesheets',
+                day: 'Tuesday',
+                reminder: true,
+            },
+            {
+                id: 3,
+                text: 'Add interactivity',
+                day: 'Wednesday',
+                reminder: false,
+            },
+        ]
+    )
+    
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
+        </>
+        )
+}
+
+export default Tasks
