79d7f1b235f03f669757a036c00970f769e304ac
Ray-Shakex <bshakex@gmail.com>
2023-08-12 10:47:35 +0200

    react-icons

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  // TASKS ARRAY
   const [tasks, setTasks] = useState(
     [{
         id: 1,
-        text: 'Write Markup',
+        text: 'HTML-5 Markup',
         day: 'Monday',
         reminder: true,
       },
       {
         id: 2,
-        text: 'Stylesheets',
+        text: 'Sass Stylesheets',
         day: 'Tuesday',
         reminder: true,
       },
       {
         id: 3,
-        text: 'Add interactivity',
+        text: 'Javascript..Scripts',
         day: 'Wednesday',
         reminder: false,
       },
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,14 @@
+// IMPORT AND USE ICONS AS REACT COMPONENT VIA REACT-ICONS
+// REACT-ICONS/FA = FONT-AWESOME ICONS LIBRARY
+import { FaTimes } from 'react-icons/fa'
+
 const Task = ({ task }) => {
     return (
         <div className="task">
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} <FaTimes style={{ 
+                    color: 'red', cursor: 'pointer' }} /> 
+            </h3>
             <p>{task.day}</p>
         </div>
     )
