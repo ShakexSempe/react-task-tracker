0ee39de944f4afba8b16d8d946328340182ea946
Ray-Shakex <bshakex@gmail.com>
2023-08-13 11:35:53 +0200

    deleteTask()

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks';
 
 function App() {
   // TASKS ARRAY
+  // TASK STATE AT THE TOP LEVEL TO ALLOW ALL COMPONENTS ACCESS
   const [tasks, setTasks] = useState(
     [{
         id: 1,
@@ -26,10 +27,19 @@ function App() {
     ]
   )
 
+  // DELETE TASK FUNCTION
+  // FILTER ARRAY TO SHOW ITEMS THAT DON'T.. 
+  // ..HAVE THE SAME ID AS THE TASK BEING CLICKED ON
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? 
+      (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No tasks to show')
+      }
     </div>
   );
 }
